e115000eeae10fb496cd8e20f3a763f35fd78484
Adamauer1 <adamauer1@gmail.com>
2024-07-26 12:19:50 +0200

    basic responsiveness for id pages

--- src/app/specjalisci/[id]/page.tsx
+++ src/app/specjalisci/[id]/page.tsx
@@ -1,5 +1,5 @@
 import NextImage from "next/image";
-import { Image } from "@mantine/core";
+import { AspectRatio, Flex, Image, rem, Text, Title } from "@mantine/core";
 import styles from "@/app/specjalisci/[id]/page.module.css";
 import { infoData } from "../../../data/data";
 
@@ -19,8 +19,56 @@ export default function SpecjalisciPage({
   return (
     <>
       {/* <div>{data?.id}</div> */}
-      <main className="" style={{}}>
-        <div className={styles.container}>
+      <>
+        <Flex
+          direction={{ base: "column", lg: "row" }}
+          align={{ base: "center", lg: "start" }}
+          //justify={{ lg: "space-around" }}
+          p={{ base: rem(20), lg: rem(50), xl: rem(100) }}
+          gap={{ base: rem(30) }}
+        >
+          <Title order={1} hiddenFrom="lg">
+            O specjaliście
+          </Title>
+          <Flex
+            direction={{ base: "column" }}
+            gap={{ base: rem(10) }}
+            flex={{ lg: 0.5 }}
+          >
+            <div className={styles.imageContainer}>
+              {/* <AspectRatio ratio={9 / 16}> */}
+              <Image
+                //component={NextImage}
+                src={data?.image}
+                // width={3738}
+                // height={4983}
+                w={{ lg: rem(400), xl: rem(500) }}
+                h={"auto"}
+                alt="person"
+
+                // fill={true}
+              />
+              {/* </AspectRatio> */}
+            </div>
+
+            <Title order={2}>{data?.name}</Title>
+            <Title order={3}>{data?.title}</Title>
+          </Flex>
+          <Flex direction={{ base: "column" }} flex={{ lg: 0.5 }}>
+            <div className={styles.containerRight}>
+              <Title order={1} visibleFrom="lg" fz={{ xl: rem(60) }} m={0}>
+                O specjaliście
+              </Title>
+              {data?.desc.split("\n").map((text, index) => (
+                <p key={index} className={styles.text}>
+                  {text}
+                  <br />
+                </p>
+              ))}
+            </div>
+          </Flex>
+        </Flex>
+        {/* <div className={styles.container}>
           <div className={styles.containerLeft}>
             <div className={styles.infoContainer}>
               <div className={styles.imageContainer}>
@@ -31,8 +79,8 @@ export default function SpecjalisciPage({
                   // height={4983}
                   alt="person"
                   // fill={true}
-                />
-                {/* <Image
+                /> */}
+        {/* <Image
                   className=""
                   src={data?.image}
                   alt="Logo"
@@ -40,11 +88,11 @@ export default function SpecjalisciPage({
                   sizes="lg"
                   style={{}}
                 /> */}
-              </div>
-              <h2>{data?.name}</h2>
-              {/* <h3>Prof. dr hab. n. med. i n. zdr.</h3> */}
-              <h3>{data?.title}</h3>
-            </div>
+        {/* </div>
+              <h2>{data?.name}</h2> */}
+        {/* <h3>Prof. dr hab. n. med. i n. zdr.</h3> */}
+        {/* <h3>{data?.title}</h3> */}
+        {/* </div>
           </div>
           <div className={styles.containerRight}>
             <div>
@@ -54,8 +102,8 @@ export default function SpecjalisciPage({
                   {text}
                   <br />
                 </p>
-              ))}
-              {/* <p>
+              ))} */}
+        {/* <p>
                 Prof. dr hab. n. med. i n. zdr. Jolanta Kucharska-Mazur,
                 specjalista psychiatra, specjalista psychiatrii środowiskowej.{" "}
                 <br />
@@ -81,10 +129,10 @@ export default function SpecjalisciPage({
                 <br /> Miłośniczka żeglarstwa, nurkowania, podróży i wędrówek
                 górskich.
               </p> */}
-            </div>
+        {/* </div>
           </div>
-        </div>
-      </main>
+        </div> */}
+      </>
     </>
   );
 }
