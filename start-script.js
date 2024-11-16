0ce3e1b8801c0240f6f56243160b5749650718e5
Adamauer1 <adamauer1@gmail.com>
2024-11-16 18:31:56 +0100

    npm switch

--- start-script.js
+++ start-script.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 // Run the production build of Next.js
-exec("npm run start", function (error, stdout, stderr) {
+exec("npm run dev", function (error, stdout, stderr) {
   console.log("stdout: " + stdout);
   console.log("stderr: " + stderr);
   if (error !== null) {
